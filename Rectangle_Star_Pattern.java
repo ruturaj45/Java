1ccc961d2bc302908e72ed9848ea3497f58d85a5
Ruturaj Biswal <106529938+ruturaj45@users.noreply.github.com>
2022-06-17 08:15:14 +0100

    Pattern 1st

---
+++ Rectangle_Star_Pattern.java
@@ -0,0 +1,13 @@
+public class Rectangle_Star_Pattern {
+    public static void main(String[] args) {
+        int i,j;
+        for(i=0;i<4;i++)
+        {
+            for(j=0;j<7;j++){
+            System.out.print(" *");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
